55ebd9517da787c26b689f9c644795ed9d304e10
Shibhikkiran <shibhikkiran@gmail.com>
2020-09-13 00:42:26 +0530

    added user-data script simple-webserver.sh

---
+++ ec2/user-data/simple-webserver.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -xe
+
+# To troubleshoot issues on your EC2 instance bootstrap without having to access the instance through SSH.
+# You can add code to your user-data bash script that redirects all the output both to the /var/log/user-data.log and to /dev/console. 
+# When the code is executed, you can see your user-data invocation logs in your console.
+
+
+exec > > (tee /var/log/user-data.log|logger -t user-data -s 2 > /dev/console) 2>&1
+    yum -y update
+    yum -y install httpd
+    systemctl start httpd.service
+    systemctl enable httpd.service
+    echo "Hello from Labs!" > /var/www/html/index.html
+    echo "user-data - Completed!"
+ 
